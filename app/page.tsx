66d5d134b93dc2e7b2800f9ee541e7936ff7baba
Felipe Mammoli <fmammoli@gmail.com>
2024-09-12 21:33:02 -0300

    removing text map with models

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
     <div className="font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <ClientMap></ClientMap>
-        {/* <Mapbox></Mapbox>  */}
       </main>
     </div>
   );
